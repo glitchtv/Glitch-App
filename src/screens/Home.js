5e33d0f5049dc7c3b04fb147cce491e9f17d4f5f
Priansh Shah <hello@Prianshs-MacBook-Pro.local>
2018-10-19 00:25:41 -0400

    add buttons to home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { Center } from "@builderx/utils";
 import AnimeCard from "../symbols/AnimeCard";
 
+import Button3 from "../symbols/button3";
+import Icon from "@builderx/icons";
 import { View, StyleSheet, Image, FlatList, Text } from "react-native";
 
 export default class Home extends Component {
   render() {
     return (
       <View style={styles.root}>
+        <View style={styles.rect3} />
         <View style={styles.header} />
         <Center horizontal>
           <Image
@@ -39,6 +42,12 @@ export default class Home extends Component {
           horizontal={true}
         />
         <Text style={styles.label2}>Watch Next</Text>
+        <Button3
+          style={styles.search}
+          iconType="FontAwesome"
+          iconName="search"
+        />
+        <Icon name="ios-refresh-outline" style={styles.icon} type="Ionicons" />
       </View>
     );
   }
@@ -114,5 +123,32 @@ const styles = StyleSheet.create({
     position: "absolute",
     backgroundColor: "transparent",
     color: "rgba(170,170,170,1)"
+  },
+  rect3: {
+    top: 60,
+    left: 0,
+    right: 0,
+    height: "91.81%",
+    position: "absolute",
+    backgroundColor: "rgba(34,34,34,1)",
+    opacity: 1
+  },
+  search: {
+    top: 630,
+    left: 277,
+    position: "absolute",
+    height: 69,
+    width: 69,
+    borderRadius: 100,
+    backgroundColor: "rgba(191,10,116,1)",
+    opacity: 1
+  },
+  icon: {
+    top: 13,
+    left: 17,
+    position: "absolute",
+    backgroundColor: "transparent",
+    color: "rgba(255,255,255,1)",
+    fontSize: 40
   }
 });
---
+++ src/symbols/button3.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Icon from "@builderx/icons";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+
+export default class button3 extends Component {
+  // Only for displaying symbol in BuilderX.
+  static containerStyle = {
+    height: 56,
+    width: 56,
+    defaultHeight: "fixed",
+    defaultWidth: "fixed"
+  };
+  render() {
+    return (
+      <TouchableOpacity style={[styles.root, this.props.style]}>
+        <Icon
+          style={styles.icon}
+          name={this.props.iconName ? this.props.iconName : "share-variant"}
+          type={
+            this.props.iconType ? this.props.iconType : "MaterialCommunityIcons"
+          }
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  root: {
+    elevation: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#3F51B5",
+    minWidth: 40,
+    minHeight: 40,
+    borderRadius: 28,
+    shadowColor: "#111",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.2
+  },
+  icon: {
+    alignSelf: "center",
+    color: "#fff",
+    fontSize: 24
+  }
+});
